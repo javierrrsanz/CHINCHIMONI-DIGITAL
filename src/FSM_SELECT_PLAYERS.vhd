3c79635edd07802b0cf780a407d6667f02e017e5
Nicoderecho <nicolasizquierdo25@gmail.com>
2026-01-03 13:39:17 +0100

    Revisión fsm_select_players

--- src/FSM_SELECT_PLAYERS.vhd
+++ src/FSM_SELECT_PLAYERS.vhd
@@ -39,27 +39,20 @@ architecture Behavioral of FSM_SELECT_PLAYERS is
     );
     signal state : state_type;
 
-    -- Senal interna de empezar timer
-    --signal int_timer_start : std_logic;
-
-    -- Senal interna done
-    --signal int_done : std_logic;
-    
     -- Valor de switches
     signal num_jugadores : unsigned(4 downto 0);
     signal players_reg   : std_logic_vector(4 downto 0);
 
 begin
     -- Switches a unsigned para trabajar comodo
-    num_jugadores <= unsigned(switches);
+    num_jugadores <= unsigned(switches); -- vhdl rellena con 0s a la izquierda
 
     FSM_PROC : process(clk,reset)
   begin
       if reset = '1' then
         state <= S_INIT;
         players_reg   <= (others => '0');
-        --int_timer_start <= '0';
-        --int_done <= '0';
+
       elsif clk 'event and clk='1' then
         case state is
   
@@ -71,7 +64,6 @@ begin
             end if;
           
           when S_WAIT_CONFIRM => 
-            -- Falta asignar disp_code
             if confirm = '1' then
               state <= S_CHECK;
             else    
@@ -79,7 +71,6 @@ begin
             end if;
 
           when S_CHECK => 
-            --int_timer_start <= '1'; -- Arranca el timer
             if (num_jugadores >= MIN_PLAYERS and num_jugadores <= MAX_PLAYERS) then
               state  <= S_SHOW_OK;
               players_reg <= std_logic_vector(num_jugadores(4 downto 0));
@@ -88,13 +79,11 @@ begin
             end if;
 
           when S_ERROR =>
-            --int_timer_start <= '0';
             if timeout_5s = '1' then
               state <= S_WAIT_CONFIRM;
             end if;
           
             when S_SHOW_OK =>
-            -- int_timer_start <= '0';
             if timeout_5s = '1' then
               state <= S_DONE;
             end if;
@@ -115,8 +104,8 @@ begin
   done <= '1' when state = S_DONE else '0';
 
   with state select
-    disp_code <=  CHAR_J & CHAR_U & CHAR_G & players_reg when S_WAIT_CONFIRM,
-                  CHAR_J & CHAR_U & CHAR_G & players_reg when S_SHOW_OK, -- Aqui hay q poner el registro en realidad
+    disp_code <=  CHAR_J & CHAR_U & CHAR_G & std_logic_vector(num_jugadores) when S_WAIT_CONFIRM, -- Aqui muestra switches
+                  CHAR_J & CHAR_U & CHAR_G & players_reg when S_SHOW_OK, -- Aqui muestra registro
                   CHAR_J & CHAR_U & CHAR_G & CHAR_BLANK when S_ERROR,
                   CHAR_BLANK & CHAR_BLANK & CHAR_BLANK & CHAR_BLANK when others;
 
